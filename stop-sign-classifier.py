c11dea631fd656eb23d20ac8877ef8d41e3cc715
matthewghuang <matthewhhuang@gmail.com>
2021-03-31 15:03:09 -0700

    improve on classification

--- stop-sign-classifier.py
+++ stop-sign-classifier.py
@@ -1,26 +1,46 @@
-# %%
 import cv2 as cv
+import numpy as np
 
-# %%
 stop_cascade = cv.CascadeClassifier()
 
 if not stop_cascade.load(cv.samples.findFile("Stopsign_HAAR_19Stages.xml")):
 	print("Error loading stop cascade")
 	exit(0)
-# %%
-def detect(frame):
-	frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-	frame_gray = cv.equalizeHist(frame_gray)
 
-	stop_signs = stop_cascade.detectMultiScale(frame_gray)
+def detect_and_draw(frame):
+	gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+	gray_equalized = cv.equalizeHist(gray)
+	b, g, r = cv.split(frame)
+	r_equalized = cv.equalizeHist(r)
+	r_equalized_inverted = cv.bitwise_not(r_equalized)
+
+	cv.imshow("r", r)
+	cv.imshow("r_equalized", r_equalized)
+	cv.imshow("r_equalized_inverted", r_equalized_inverted)
+	cv.imshow("g", gray)
+	cv.imshow("g_equalized", gray_equalized)
+	
+	stop_signs = stop_cascade.detectMultiScale(r_equalized_inverted)
 
 	for (x, y, w, h) in stop_signs:
 		size = w * h
-		frame = cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
-		frame = cv.putText(frame, "size: %x" % size, (x, y), cv.FONT_HERSHEY_SIMPLEX, (0, 255, 0), 2)
-		cv.imshow("Stop Sign Classification", frame)
+		frame = cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0))
+		frame = cv.putText(frame, "size: %i" % size, (x, y), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0))
 
-# %%
+	cv.imshow("Classifier", frame)
+
+	# frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+	# frame_gray = cv.equalizeHist(frame_gray)
+
+	# stop_signs = stop_cascade.detectMultiScale(frame_gray)
+
+	# for (x, y, w, h) in stop_signs:
+	# 	size = w * h
+	# 	frame = cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0))
+	# 	frame = cv.putText(frame, "size: %i" % size, (x, y), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0))
+
+	# cv.imshow("Classifier", frame)
+
 camera = cv.VideoCapture(0)
 
 if not camera.isOpened:
@@ -34,7 +54,9 @@ while True:
 		print("No frame captured")
 		break
 
-	detect(frame)
+	detect_and_draw(frame)
+	# cv.imshow("test", frame)
 
-	if cv.waitKey(10) == 27:
+	if cv.waitKey(50) == 27:
+		cv.destroyAllWindows()
 		break
