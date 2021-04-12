1181da4bc62e882e78089b592f32992488cc9151
matthewghuang <matthewhhuang@gmail.com>
2021-04-12 21:34:09 +0100

    add self driving

--- gpio.py
+++ gpio.py
@@ -1,8 +1,45 @@
 from gpiozero import DistanceSensor
-import time
+from time import sleep
+import socketio
 
-distance_sensor = DistanceSensor(echo=3, trigger=2)
+sio = socketio.Client()
+sio.connect("ws://localhost:3000")
 
-while True:
-    print("Distance: {}".format(distance_sensor.distance))
-    time.sleep(1)
+distance_sensor = DistanceSensor(echo=21, trigger=20, max_distance=1)
+
+threshold = 0.3
+
+previous_action = "right"
+
+def move(direction):
+    sio.emit("set_direction", direction)
+
+def main():
+    global previous_action
+
+    while True:
+        distance = distance_sensor.distance
+
+        if distance >= threshold:
+            move("up")
+            sleep(0.5)
+            move("")
+        else:
+            if previous_action == "right":
+                move("left")
+                sleep(0.5)
+                move("")
+                previous_action = "left"
+                print("moving left to avoid obstacle")
+            elif previous_action == "left":
+                move("right")
+                sleep(1)
+                move("")
+                previous_action = "right"
+                print("moving right to avoid obstacle")
+
+        print("Distance: {}".format(distance_sensor.distance))
+        sleep(0.1)
+
+if __name__ == "__main__":
+    main()
